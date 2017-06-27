c01643392f58037a0186d2f9fb555ea092cf53d2
Stytan <stytanus@gmail.com>
2017-06-27 12:06:39 +0300

    Update ExchangeClient.cs

--- DZ_10_client/ExchangeClient.cs
+++ DZ_10_client/ExchangeClient.cs
@@ -1,16 +1,25 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
 
 namespace DZ_10_client
 {
+	public enum ExDirection
+	{
+		EURbuy, EURsale, RUBbuy, RUBsale, USDbuy, USDsale
+	}
     class ExchangeClient
     {
+   		private string[] DirStr = new string[]
+   			{"EURbuy", "EURsale", "RUBbuy", "RUBsale", "USDbuy", "USDsale"};
         private Socket clientSocket;
         private IPEndPoint ep;
         private StreamReader sr;
         private StreamWriter sw;
+        /// <summary>
+        /// Конструктор задаёт параметры подключения
+        /// </summary>
         public ExchangeClient()
         {
             clientSocket = new Socket(
@@ -20,16 +29,25 @@ namespace DZ_10_client
             IPAddress ip = IPAddress.Parse("127.0.0.1");
             ep = new IPEndPoint(ip, 13853);
         }
+        /// <summary>
+        /// Выполняет подключение к серверу
+        /// </summary>
         public void Connect()
         {
             clientSocket.Connect(ep);
             sr = new StreamReader(new NetworkStream(clientSocket));
             sw = new StreamWriter(new NetworkStream(clientSocket));
         }
-        public double Convert(string direct, double money)
+        /// <summary>
+        /// Конвертирует валюты
+        /// </summary>
+        /// <param name="direct"></param>
+        /// <param name="money"></param>
+        /// <returns></returns>
+        public double Convert(ExDirection direct, double money)
         {
             double res = 0;
-            sw.WriteLine(direct + ' ' + money.ToString());
+            sw.WriteLine(DirStr[(int)direct] + ' ' + money.ToString());
             sw.Flush();
             string tmp = sr.ReadLine();
             if (!tmp.Equals("error"))
@@ -40,6 +58,9 @@ namespace DZ_10_client
                 throw new ApplicationException("Ошибка конвертации. Возможно не верный формат числа.");
             return res;
         }
+        /// <summary>
+        /// Закрывает подключение к серверу
+        /// </summary>
         public void Close()
         {
             sw.WriteLine("/disconnect");
